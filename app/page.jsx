730bce16955f8ef8dac1acd30a8819a08aa353ec
Sahil Suman <sahilsuman1202@gmail.com>
2025-03-11 22:44:35 +0530

    fixed typo

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     return (
       <section className="w-full flex-center flex-col">  
         <h1 className="head_text text-center">
-          Discover & Share
+          Discover & Share 
           <br className="max-md:hidden"/>
-          <span className="orange_gradient">AI-Powered Prompts</span>
+          <span className="orange_gradient"> AI-Powered Prompts</span>
         </h1>
 
         <p className="desc text-center">
