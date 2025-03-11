2ed44bc14c5e0f7380f831122d05188467bf8118
Sahil Suman <sahilsuman1202@gmail.com>
2025-03-11 22:47:19 +0530

    fixing layout

--- app/page.jsx
+++ app/page.jsx
@@ -7,6 +7,7 @@ const Home = () => {
         <h1 className="head_text text-center">
           Discover & Share 
           <br className="max-md:hidden"/>
+          <br/>
           <span className="orange_gradient"> AI-Powered Prompts</span>
         </h1>
 
